a5406769bcdccefb8edccb7a430adbd746f0034d
Fernando Gruning <fernandojgruning@gmail.com>
2020-12-28 19:35:44 -0400

    feat: add project workspaces support
    - Refactor project's npm scripts
    - Change author

--- hello.js
+++
@@ -1,3 +0,0 @@
-const hello = () => "hello world";
-
-module.exports = hello;
--- hello.test.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require("@jest/globals");
-const hello = require("./hello");
-
-test('returns "hello world"', () => {
-  expect(hello()).toBe("hello world");
-});
