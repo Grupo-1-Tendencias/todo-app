ad1157149c31e064db9f7fa5c8a5b8ea5d4e2830
Raúl Luna <enmanuellperez98@gmail.com>
2021-01-12 21:50:00 -0400

    fix: Layout and Styles

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,10 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">To Do</header>
-      <div data-testid="body" className="App-body"></div>
+      <main role="main">
+        <header className="App-header">To Do</header>
+        <div data-testid="body" className="App-body"></div>
+      </main>
     </div>
   );
 }
