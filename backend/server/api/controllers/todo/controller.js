7fdfe4949d675ab46209eff1c6793b1be3ec395e
Kevin Rosario <kevinrosariocruz@outlook.com>
2021-01-13 10:07:40 -0400

    kevin/create-controller-dir: changes in todo/controller.js and api.yml updated

--- backend/server/api/controllers/todo/controller.js
+++ backend/server/api/controllers/todo/controller.js
@@ -1,10 +1,2 @@
-const service = require("../../services/todos.service");
-
-export class Controller {
-	create(req, res) {
-		ExamplesService.create(req.body.name).then((r) =>
-			res.status(201).location(`/api/examples/${r.id}`).json(r)
-		);
-	}
-} //CRUDS methods will be in this class
+export class Controller {}
 export default new Controller();
