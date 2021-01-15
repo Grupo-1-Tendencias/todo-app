ea4db3dc8fcf59bae8b464f093b8beae76592b88
Kevin Rosario <kevinrosariocruz@outlook.com>
2021-01-15 08:54:32 -0400

    fourth test case for create controller method passed

--- backend/test/todo.controller.js
+++ backend/test/todo.controller.js
@@ -5,86 +5,86 @@ import Server from "../server";
 const expect = chai.expect;
 
 console.log(
-	`This is temporal, please ignore:... ${chai.version} ${request.name} ${Server} ${expect.name}...`
+  `This is temporal, please ignore:... ${chai.version} ${request.name} ${Server} ${expect.name}...`
 );
 
 describe("Stub test for controller and server", () => {
-	it("should send an object to controller.stub and receive the same object", () =>
-		request(Server)
-			.post("/api/todo/stub")
-			.send({ name: "test" })
-			.expect("Content-Type", /json/)
-			.then((r) => {
-				expect(r.body)
-					.to.be.an.an("object")
-					.that.has.property("name")
-					.equal("test");
-			}));
+  it("should send an object to controller.stub and receive the same object", () =>
+    request(Server)
+      .post("/api/todo/stub")
+      .send({ name: "test" })
+      .expect("Content-Type", /json/)
+      .then((r) => {
+        expect(r.body)
+          .to.be.an.an("object")
+          .that.has.property("name")
+          .equal("test");
+      }));
 });
 
 describe("test cases for create controller method", () => {
-	it("should add a new todo when body has all properties", () => {
-		const todo = {
-			name: "French homework",
-			description: "Pages 23 and 24 exercises 1-9",
-			isDone: false,
-			dueDate: "20-04-2021",
-		};
-		return request(Server)
-			.post("/api/todo")
-			.send(todo)
-			.then((r) => {
-				expect(r.statusCode).to.equal(201);
-			});
-	});
+  it("should add a new todo when body has all properties", () => {
+    const todo = {
+      name: "French homework",
+      description: "Pages 23 and 24 exercises 1-9",
+      isDone: false,
+      dueDate: "20-04-2021",
+    };
+    return request(Server)
+      .post("/api/todo")
+      .send(todo)
+      .then((r) => {
+        expect(r.statusCode).to.equal(201);
+      });
+  });
 
-	it("should send bad request when attempt to add new todo with missing name property", () => {
-		const todo = {
-			description: "Pages 23 and 24 exercises 1-9",
-			dueDate: "20-04-2021",
-			isDone: false,
-		};
-		return request(Server)
-			.post("/api/todo")
-			.send(todo)
-			.then((r) => {
-				expect(r.statusCode).to.equal(400);
-			});
-	});
+  it("should send bad request when attempt to add new todo with missing name property", () => {
+    const todo = {
+      description: "Pages 23 and 24 exercises 1-9",
+      dueDate: "20-04-2021",
+      isDone: false,
+    };
+    return request(Server)
+      .post("/api/todo")
+      .send(todo)
+      .then((r) => {
+        expect(r.statusCode).to.equal(400);
+      });
+  });
 
-	it("should send description as an empty string when attempting to add new todo with missing description property", () => {
-		const todo = {
-			name: "Do french homework",
-			dueDate: "20-04-2021",
-			isDone: false,
-		};
-		return request(Server)
-			.post("/api/todo")
-			.send(todo)
-			.then((r) => {
-				expect(r.statusCode).to.equal(201);
-				expect(r.body)
-					.to.be.an.an("object")
-					.that.has.property("description")
-					.equal("");
-			});
-	});
+  it("should send description as an empty string when attempting to add new todo with missing description property", () => {
+    const todo = {
+      name: "Do french homework",
+      dueDate: "20-04-2021",
+      isDone: false,
+    };
+    return request(Server)
+      .post("/api/todo")
+      .send(todo)
+      .then((r) => {
+        expect(r.statusCode).to.equal(201);
+        expect(r.body)
+          .to.be.an.an("object")
+          .that.has.property("description")
+          .equal("");
+      });
+  });
 
-	it("should send dueDate as an empty string when attempting to add new todo with missing dueDate property", () => {
-		const todo = {
-			name: "Do french homework",
-			description: "Do exercises in pages 24 and 25",
-			isDone: false,
-		};
-		return request(Server)
-			.post("/api/todo")
-			.send(todo)
-			.then((r) => {
-				expect(r.statusCode).to.equal(201);
-				expect(r.body)
-					.to.be.an.an("object")
-					.that.has.property("dueDate")
-					.equal("");
-			});
-	});
+  it("should send dueDate as an empty string when attempting to add new todo with missing dueDate property", () => {
+    const todo = {
+      name: "Do french homework",
+      description: "Do exercises in pages 24 and 25",
+      isDone: false,
+    };
+    return request(Server)
+      .post("/api/todo")
+      .send(todo)
+      .then((r) => {
+        expect(r.statusCode).to.equal(201);
+        expect(r.body)
+          .to.be.an.an("object")
+          .that.has.property("dueDate")
+          .equal("");
+      });
+  });
 });
