076ea382ce4f235410c58d6fb1d80ce64a850d17
Kevin Rosario <kevinrosariocruz@outlook.com>
2021-01-14 23:43:28 -0400

    refactor and changes in create controller method to accept empty descriptions

--- backend/server/api/controllers/todo/controller.js
+++ backend/server/api/controllers/todo/controller.js
@@ -1,31 +1,33 @@
 import ToDoService from "../../services/todos.service";
 
 export class Controller {
-  stub(req, res) {
-    //	console.log(`Temporal... ${db}...`);
-    return res.json(req.body);
-  }
-  create(req, res) {
-    const todo = {
-      name: req.body.name,
-      description: req.body.description,
-      isDone: false,
-      dueDate: req.body.dueDate,
-    };
-    if (todo.name == undefined) {
-      //If name of todo is undefined
-      res.status(400).send();
-    } else {
-      try {
-        ToDoService.db.ref("todo").push(todo);
-        //db.db.ref("todo").push(todo);
-        res.status(201).send();
-      } catch (error) {
-        console.log(error.name + error.message);
-        res.status(400).send();
-      }
-    }
-  }
+	stub(req, res) {
+		//	console.log(`Temporal... ${db}...`);
+		return res.json(req.body);
+	}
+	create(req, res) {
+		const todo = {
+			name: req.body.name,
+			description: req.body.description,
+			isDone: false,
+			dueDate: req.body.dueDate,
+		};
+		if (todo.description == undefined) {
+			//If no description was provided, then set it equal to an empty string
+			//so the data base accepts an empty description
+			todo.description = "";
+		}
+		if (todo.name == undefined) {
+			res.status(400).send();
+		} else {
+			try {
+				ToDoService.db.ref("todo").push(todo);
+				res.status(201).send();
+			} catch (error) {
+				res.status(500).send();
+			}
+		}
+	}
 }
 
 export default new Controller();
