caee8a44719e9b024cacfe1cb9632d44de671d4a
Kevin Rosario <kevinrosariocruz@outlook.com>
2021-01-14 23:25:32 -0400

    test cases 1 and 2 for create method passed and refactored

--- backend/server/api/controllers/todo/controller.js
+++ backend/server/api/controllers/todo/controller.js
@@ -1,24 +1,31 @@
-const db = require("../../services/todos.service");
+import ToDoService from "../../services/todos.service";
 
 export class Controller {
-	stub(req, res) {
-		console.log(`Temporal... ${db}...`);
-		return res.json(req.body);
-	}
-	create(req, res) {
-		const todo = {
-			name: req.body.name,
-			description: req.body.description,
-			isDone: req.body.isDone,
-			dueDate: req.body.dueDate,
-		};
-		if (todo.name == false) {
-			//If required properties are falsy
-			res.status(400).send();
-		} else {
-			res.status(201).send();
-		}
-	}
+  stub(req, res) {
+    //	console.log(`Temporal... ${db}...`);
+    return res.json(req.body);
+  }
+  create(req, res) {
+    const todo = {
+      name: req.body.name,
+      description: req.body.description,
+      isDone: false,
+      dueDate: req.body.dueDate,
+    };
+    if (todo.name == undefined) {
+      //If name of todo is undefined
+      res.status(400).send();
+    } else {
+      try {
+        ToDoService.db.ref("todo").push(todo);
+        //db.db.ref("todo").push(todo);
+        res.status(201).send();
+      } catch (error) {
+        console.log(error.name + error.message);
+        res.status(400).send();
+      }
+    }
+  }
 }
 
 export default new Controller();
--- backend/server/api/controllers/todo/router.js
+++ backend/server/api/controllers/todo/router.js
@@ -5,3 +5,4 @@ export default express
   .Router()
   .post("/", controller.create)
   .post("/stub", controller.stub);
+//.delete("/DELETE/:id", controller.deleteByID);
