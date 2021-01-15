be7ae1ba100e19ee6761540baf22703c85248881
Kevin Rosario <kevinrosariocruz@outlook.com>
2021-01-14 21:16:47 -0400

    refactor for create controller method and first test case

--- backend/server/api/controllers/todo/controller.js
+++ backend/server/api/controllers/todo/controller.js
@@ -1,21 +1,19 @@
 const db = require("../../services/todos.service");
 
 export class Controller {
-  stub(req, res) {
-    console.log(`Temporal... ${db}...`);
-    return res.json(req.body);
-  }
-  create(req, res) {
-    const todo = {
-      name: req.body.name,
-      description: req.body.description,
-      isDone: req.body.isDone,
-      dueDate: req.body.dueDate,
-    };
-    res
-      .status(201)
-      .send({ message: `Todo: "${todo.name} added to data base succesfully"` });
-  }
+	stub(req, res) {
+		console.log(`Temporal... ${db}...`);
+		return res.json(req.body);
+	}
+	create(req, res) {
+		const todo = {
+			name: req.body.name,
+			description: req.body.description,
+			isDone: req.body.isDone,
+			dueDate: req.body.dueDate,
+		};
+		res.status(201).send();
+	}
 }
 
 export default new Controller();
--- backend/test/todo.controller.js
+++ backend/test/todo.controller.js
@@ -5,38 +5,36 @@ import Server from "../server";
 const expect = chai.expect;
 
 console.log(
-  `This is temporal, please ignore:... ${chai.version} ${request.name} ${Server} ${expect.name}...`
+	`This is temporal, please ignore:... ${chai.version} ${request.name} ${Server} ${expect.name}...`
 );
 
 describe("Stub test for controller and server", () => {
-  it("should send an object to controller.stub and receive the same object", () =>
-    request(Server)
-      .post("/api/todo/stub")
-      .send({ name: "test" })
-      .expect("Content-Type", /json/)
-      .then((r) => {
-        expect(r.body)
-          .to.be.an.an("object")
-          .that.has.property("name")
-          .equal("test");
-      }));
+	it("should send an object to controller.stub and receive the same object", () =>
+		request(Server)
+			.post("/api/todo/stub")
+			.send({ name: "test" })
+			.expect("Content-Type", /json/)
+			.then((r) => {
+				expect(r.body)
+					.to.be.an.an("object")
+					.that.has.property("name")
+					.equal("test");
+			}));
 });
 
 describe("test cases for create controller method", () => {
-  it("should add a new todo when body has all properties", () => {
-    const todo = {
-      name: "French homework",
-      description: "Pages 23 and 24 exercises 1-9",
-      isDone: false,
-      dueDate: "20-04-2021",
-    };
-    return request(Server)
-      .post("/api/todo")
-      .send(todo)
-      .expect("Content-Type", /json/)
-      .then((r) => {
-        expect(r.statusCode).to.equal(201);
-        expect(r.body).to.have.a.property("message");
-      });
-  });
+	it("should add a new todo when body has all properties", () => {
+		const todo = {
+			name: "French homework",
+			description: "Pages 23 and 24 exercises 1-9",
+			isDone: false,
+			dueDate: "20-04-2021",
+		};
+		return request(Server)
+			.post("/api/todo")
+			.send(todo)
+			.then((r) => {
+				expect(r.statusCode).to.equal(201);
+			});
+	});
 });
