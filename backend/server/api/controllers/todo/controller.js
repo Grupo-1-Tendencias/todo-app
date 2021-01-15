a1350d34d6c678a1f6b087633da53148a90b9de1
Kevin Rosario <kevinrosariocruz@outlook.com>
2021-01-15 09:12:08 -0400

    fifth test case for create controller method passed

--- backend/server/api/controllers/todo/controller.js
+++ backend/server/api/controllers/todo/controller.js
@@ -1,27 +1,27 @@
 import ToDoService from "../../services/todos.service";
 
 export class Controller {
-	stub(req, res) {
-		return res.json(req.body);
-	}
-	create(req, res) {
-		const todo = {
-			name: req.body.name,
-			description: req.body.description || "",
-			isDone: req.body.isDone,
-			dueDate: req.body.dueDate || "",
-		};
-		if (todo.name == undefined) {
-			res.status(400).send();
-		} else {
-			try {
-				ToDoService.db.ref("todo").push(todo);
-				res.status(201).json(todo).send();
-			} catch (error) {
-				res.status(500).send();
-			}
-		}
-	}
+  stub(req, res) {
+    return res.json(req.body);
+  }
+  create(req, res) {
+    const todo = {
+      name: req.body.name,
+      description: req.body.description || "",
+      isDone: req.body.isDone || false,
+      dueDate: req.body.dueDate || "",
+    };
+    if (todo.name == undefined) {
+      res.status(400).send();
+    } else {
+      try {
+        ToDoService.db.ref("todo").push(todo);
+        res.status(201).json(todo).send();
+      } catch (error) {
+        res.status(500).send();
+      }
+    }
+  }
 }
 
 export default new Controller();
--- backend/server/api/controllers/todo/router.js
+++ backend/server/api/controllers/todo/router.js
@@ -2,6 +2,6 @@ import * as express from "express";
 import controller from "./controller";
 
 export default express
-	.Router()
-	.post("/", controller.create)
-	.post("/stub", controller.stub);
+  .Router()
+  .post("/", controller.create)
+  .post("/stub", controller.stub);
