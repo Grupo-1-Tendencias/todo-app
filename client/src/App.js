3d40cafc23e521ca0e9312bd445874691af3362d
fernandogruning <fernandojgruning@gmail.com>
2021-01-17 19:26:13 -0400

    format

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
-import {Update} from "./Update"
+import { Update } from "./Update";
 
 library.add(faTrash, faEdit, faPlus);
 
--- client/src/Update.js
+++ client/src/Update.js
@@ -1,71 +1,90 @@
 import React, { Component } from "react";
 export default class AddTodo extends Component {
-    state = {
-        Id: "",
-        Title: "",
-        Description: "",
-        Status: "Pending"
-    };
-handleIdChange = (event) => {
-        this.setState({
-            Id: event.target.value
-        });
-    };
-    handleTitleChange = (event) => {
-        this.setState({
-            Title: event.target.value
-        });
-    }
-    handleDescriptionChange = (event) => {
-        this.setState({
-            Description: event.target.value
-        });
-    }
-    handleStatusChange = (event) => {
-        this.setState({
-            Status: event.target.value
-        });
-    };
-    handleToDoSubmit = (event) => {
-        event.preventDefault();
-        this.props.onAdd({
-            Id: this.state.Id,
-            Title: this.state.Title,
-            Description: this.state.Description,
-            Status: this.state.Status
-        });
-        this.setState({
-            Id: "",
-            Title: "",
-            Description: "",
-            Status: "Pending"
-        });
-    };
-    render() {
-        return (
-            <div>
-                <h3>
-                    Update ToDo
-                </h3>
-                <form onSubmit={this.handleToDoSubmit} >
-                    <div className="form-group" >
-                        <input value={this.state.Id} onChange={this.handleIdChange} className="form-control" placeholder="Enter Id" />
-                    </div>
-                    <div className="form-group" >
-                        <input value={this.state.Title} onChange={this.handleTitleChange} className="form-control" placeholder="Enter Title" />
-                    </div>
-                    <div className="form-group" >
-                        <input value={this.state.Description} onChange={this.handleDescriptionChange} className="form-control" placeholder="Enter Endter Description" />
-                    </div>
-                    <div className="form-group">
-                        <select value={this.state.Status} onChange={this.handleStatusChange} className="form-control" >
-                            <option value="Done" >Done</option>
-                            <option value="Pending" >Pending</option>
-                        </select>
-                    </div>
-                    <button type="submit" className="form-control btn btn-primary" >Save</button>
-                </form>
-            </div>
-        );
-    }
+  state = {
+    Id: "",
+    Title: "",
+    Description: "",
+    Status: "Pending",
+  };
+  handleIdChange = (event) => {
+    this.setState({
+      Id: event.target.value,
+    });
+  };
+  handleTitleChange = (event) => {
+    this.setState({
+      Title: event.target.value,
+    });
+  };
+  handleDescriptionChange = (event) => {
+    this.setState({
+      Description: event.target.value,
+    });
+  };
+  handleStatusChange = (event) => {
+    this.setState({
+      Status: event.target.value,
+    });
+  };
+  handleToDoSubmit = (event) => {
+    event.preventDefault();
+    this.props.onAdd({
+      Id: this.state.Id,
+      Title: this.state.Title,
+      Description: this.state.Description,
+      Status: this.state.Status,
+    });
+    this.setState({
+      Id: "",
+      Title: "",
+      Description: "",
+      Status: "Pending",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h3>Update ToDo</h3>
+        <form onSubmit={this.handleToDoSubmit}>
+          <div className="form-group">
+            <input
+              value={this.state.Id}
+              onChange={this.handleIdChange}
+              className="form-control"
+              placeholder="Enter Id"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              value={this.state.Title}
+              onChange={this.handleTitleChange}
+              className="form-control"
+              placeholder="Enter Title"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              value={this.state.Description}
+              onChange={this.handleDescriptionChange}
+              className="form-control"
+              placeholder="Enter Endter Description"
+            />
+          </div>
+          <div className="form-group">
+            <select
+              value={this.state.Status}
+              onChange={this.handleStatusChange}
+              className="form-control"
+            >
+              <option value="Done">Done</option>
+              <option value="Pending">Pending</option>
+            </select>
+          </div>
+          <button type="submit" className="form-control btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
