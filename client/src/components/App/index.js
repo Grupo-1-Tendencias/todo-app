517325be722fa93654e49f4d46c8d069a653bce5
fernandogruning <fernandojgruning@gmail.com>
2021-01-17 18:34:13 -0400

    refactor(client): structure components directory

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
+import "./styles.css";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "../../pages/Home";
 
 function App() {
   return (
