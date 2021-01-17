d8570a074fcfdb1838ca77128166e97a64cd21ae
fernandogruning <fernandojgruning@gmail.com>
2021-01-17 19:41:15 -0400

    fix(client): actually use Update component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
-import { Update } from "./Update";
+import Update from "./Update";
 
 library.add(faTrash, faEdit, faPlus);
 
@@ -11,7 +11,13 @@ function App() {
     <div className="App">
       <main role="main">
         <header className="App-header">To Do</header>
-        <div data-testid="body" className="App-body"></div>
+        <div data-testid="body" className="App-body">
+          <Update
+            onAdd={(data) => {
+              console.log(data);
+            }}
+          />
+        </div>
       </main>
     </div>
   );
