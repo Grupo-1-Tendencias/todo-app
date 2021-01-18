73bc00a6564fc03904c569daf21bb68a0f0186c0
fernandogruning <fernandojgruning@gmail.com>
2021-01-18 16:16:39 -0400

    fix(test): remove stub tests

--- backend/test/todo.controller.js
+++ backend/test/todo.controller.js
@@ -4,20 +4,6 @@ import Server from "../server";
 
 const expect = chai.expect;
 
-describe("Stub test for controller and server", () => {
-  it("should send an object to controller.stub and receive the same object", () =>
-    request(Server)
-      .post("/api/todo/stub")
-      .send({ name: "test" })
-      .expect("Content-Type", /json/)
-      .then((r) => {
-        expect(r.body)
-          .to.be.an.an("object")
-          .that.has.property("name")
-          .equal("test");
-      }));
-});
-
 describe("test cases for create controller method", () => {
   it("should add a new todo when body has all properties", () => {
     const todo = {
