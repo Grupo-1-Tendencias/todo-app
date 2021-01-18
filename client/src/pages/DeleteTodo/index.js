2cffefe1af3cd933d539824b2c785d24bddb4eab
fernandogruning <fernandojgruning@gmail.com>
2021-01-18 02:01:54 -0400

    fix(client): remove unused import

--- client/src/pages/DeleteTodo/index.js
+++ client/src/pages/DeleteTodo/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useHistory, useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import Layout from "../../components/Layout";
 
 export default function DeleteTodo() {
