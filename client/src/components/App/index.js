7cc5662bc1062d6bd0e170121b9dba9149088969
fernandogruning <fernandojgruning@gmail.com>
2021-01-18 00:53:26 -0400

    feat: add update view

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./styles.css";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "../../pages/Home";
 import CreateTodo from "../../pages/CreateTodo";
+import UpdateTodo from "../../pages/UpdateTodo";
 
 function App() {
   return (
@@ -15,8 +21,15 @@ function App() {
         <Route path="/create" data-testid="route">
           <CreateTodo />
         </Route>
-        <Route path="/update/:id" data-testid="route">
-          <h1>PUT /todo/:id</h1>
+        <Route path="/update" data-testid="route">
+          <Switch>
+            <Route exact path="/update">
+              <Redirect to="/" />
+            </Route>
+            <Route path={`/update/:id`}>
+              <UpdateTodo />
+            </Route>
+          </Switch>
         </Route>
         <Route path="/detail/:id" data-testid="route">
           <h1>GET /todo/:id</h1>
