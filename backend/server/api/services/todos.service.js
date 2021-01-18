1ee3b37727bb305b9bc7c285ac88097234fd8fe2
waldoomaet <waldo.omaet@hotmail.com>
2021-01-17 20:10:56 -0400

    Prettier

--- backend/server/api/services/todos.service.js
+++ backend/server/api/services/todos.service.js
@@ -2,57 +2,66 @@ const admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 export class ToDoService {
-    constructor() {
-        var app = admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-            databaseURL: "https://grupo-1-tendencias-todo-app-default-rtdb.firebaseio.com/",
-        });
-        this.db = app.database();
-    }
+  constructor() {
+    var app = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL:
+        "https://grupo-1-tendencias-todo-app-default-rtdb.firebaseio.com/",
+    });
+    this.db = app.database();
+  }
 
-    async create(todo) {
-        const newTodoRef = await this.db.ref("todo").push();
-        await newTodoRef.set(todo);
-        const allTodos = await this.db.ref("todo").once("value");
-        const newTodo = allTodos.child(newTodoRef.key).toJSON();
-        return {...newTodo, key: newTodoRef.key };
-    }
+  async create(todo) {
+    const newTodoRef = await this.db.ref("todo").push();
+    await newTodoRef.set(todo);
+    const allTodos = await this.db.ref("todo").once("value");
+    const newTodo = allTodos.child(newTodoRef.key).toJSON();
+    return { ...newTodo, key: newTodoRef.key };
+  }
 
-    async search(todoName) {
-        let matchTodos = [];
-        await this.db.ref("todo").orderByChild("name").equalTo(todoName).once("value", function(snapshot) {
-            matchTodos = Object.values(snapshot.val());
-            return matchTodos;
-        }, function(errorObject) {
-            console.log("The read failed: " + errorObject.code);
-            return -1;
-        });
-        // Just in case
-        return matchTodos;
-    }
-
-    async all() {
-        const allTodos = await this.db.ref("todo").once("value");
-        var arr = [];
-        allTodos.forEach((snapshot) => {
-            arr.push(snapshot.val());
-        });
-        return arr;
-    }
-
-    async byId(id) {
-        const todo = await this.db.ref("todo/" + id).once("value");
-        return todo.val();
-    }
-
-    async deleteByID(key) {
-        const ref = await this.db.ref("todo/" + key);
-        const todo = await ref.once("value");
-        if (todo.val() != null) {
-            ref.remove();
-            return true;
+  async search(todoName) {
+    let matchTodos = [];
+    await this.db
+      .ref("todo")
+      .orderByChild("name")
+      .equalTo(todoName)
+      .once(
+        "value",
+        function (snapshot) {
+          matchTodos = Object.values(snapshot.val());
+          return matchTodos;
+        },
+        function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+          return -1;
         }
+      );
+    // Just in case
+    return matchTodos;
+  }
+
+  async all() {
+    const allTodos = await this.db.ref("todo").once("value");
+    var arr = [];
+    allTodos.forEach((snapshot) => {
+      arr.push(snapshot.val());
+    });
+    return arr;
+  }
+
+  async byId(id) {
+    const todo = await this.db.ref("todo/" + id).once("value");
+    return todo.val();
+  }
+
+  async deleteByID(key) {
+    const ref = await this.db.ref("todo/" + key);
+    const todo = await ref.once("value");
+    if (todo.val() != null) {
+      ref.remove();
+      return true;
     }
+  }
 }
 
 export default new ToDoService();
