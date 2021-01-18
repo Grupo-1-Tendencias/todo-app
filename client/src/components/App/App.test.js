675b0ac5d624b4010b3749c90076c7f93ef9c22c
MichelleVic <67714999+MichelleVic@users.noreply.github.com>
2021-01-18 00:53:53 -0400

    Add Read frontend (#31)
    Co-authored-by: Lisa Vicente <lisamvicentea@gmail.com>
    Co-authored-by: fernandogruning <fernandojgruning@gmail.com>

--- client/src/components/App/App.test.js
+++ client/src/components/App/App.test.js
@@ -11,13 +11,23 @@ test("renders header and body", () => {
   expect(body).toBeInTheDocument();
 });
 
-// // test("renders input fields", () => {
-// //   render(<TodoWrapper />);
-// //   const nameField = screen.getByPlaceholderText("Enter title");
-// //   const descriptionField = screen.getAllByPlaceholderText("Enter description");
-// //   const dueDateField = screen.getByPlaceholderText("Enter due date");
+// test("renders API items", async () => {
+//   render(<App />);
 
-// //   expect(nameField).toBeInTheDocument();
-// //   expect(descriptionField).toBeInTheDocument();
-// //   expect(dueDateField).toBeInTheDocument();
+//   const items = await screen.findAllByTestId("api-item");
+//   items.forEach((item) => {
+//     expect(item).toBeInTheDocument();
+//   });
+//   cleanup();
+// });
+
+// test("renders input fields", () => {
+//   render(<TodoWrapper />);
+//   const nameField = screen.getByPlaceholderText("Enter title");
+//   const descriptionField = screen.getAllByPlaceholderText("Enter description");
+//   const dueDateField = screen.getByPlaceholderText("Enter due date");
+
+//   expect(nameField).toBeInTheDocument();
+//   expect(descriptionField).toBeInTheDocument();
+//   expect(dueDateField).toBeInTheDocument();
 // });
